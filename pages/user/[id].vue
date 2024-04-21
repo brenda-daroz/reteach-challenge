5e564447d3e31a15f09b490ac31e841af7a62d4f
Brenda Daroz <brenda.daroz@gmail.com>
2024-04-21 13:47:30 +0200

    run prettier

--- pages/user/[id].vue
+++ pages/user/[id].vue
@@ -12,11 +12,11 @@
 <script setup lang="ts">
 type UserDetails = {
   id: number;
-  name: string
-}
-const route = useRoute()
-const { data: userDetails, pending, error } = await useFetch<UserDetails>(`https://jsonplaceholder.typicode.com/users/${route.params.id}`)
-console.log(userDetails.value?.id)
+  name: string;
+};
+const route = useRoute();
+const { data: userDetails, pending } = await useFetch<UserDetails>(
+  `https://jsonplaceholder.typicode.com/users/${route.params.id}`
+);
+console.log(userDetails.value?.id);
 </script>
-
-<style></style>
