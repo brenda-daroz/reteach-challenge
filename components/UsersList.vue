07e35bb788a220a51d3aff273636ff674bc1402e
Brenda Daroz <brenda.daroz@gmail.com>
2024-04-21 13:48:37 +0200

    adapt api call for spa

--- components/UsersList.vue
+++ components/UsersList.vue
@@ -1,32 +1,42 @@
 <template>
   <AwesomeSection>
     <h2>Users List</h2>
-    <input type="text" placeholder="Search..." v-model="search" />
-    <ul v-for="user in filteredUsers" :key="user.id">
-      <NuxtLink :to="`/user/${user.id}`" tag="li">
-        <p>Name: {{ user.name }}</p>
-        <p>Email: {{ user.email }}</p>
-        <p>Username: {{ user.username }}</p>
-        <hr />
-      </NuxtLink>
-    </ul>
+    <p v-if="pending">Loading...</p>
+    <div v-else>
+      <input type="text" placeholder="Search..." v-model="search" />
+      <ul v-for="user in filteredUsers" :key="user.id">
+        <NuxtLink :to="`/user/${user.id}`" tag="li">
+          <p>Name: {{ user.name }}</p>
+          <p>Email: {{ user.email }}</p>
+          <p>Username: {{ user.username }}</p>
+          <hr />
+        </NuxtLink>
+      </ul>
+    </div>
   </AwesomeSection>
 </template>
 
-<script setup>
-import { useAsyncData } from 'nuxt/app';
+<script setup lang="ts">
+import { ref, computed } from "vue";
 
-const search = ref('');
+const search = ref("");
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  username: string;
+}
 
 const filteredUsers = computed(() =>
-  users
-    ? users.value.filter((user) =>
+  users.value
+    ? users.value.filter((user: User) =>
         user.name.toLowerCase().includes(search.value.toLowerCase())
       )
     : []
 );
 
-const { data: users } = useAsyncData(() =>
-  fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json())
+const { data: users, pending } = await useFetch<User[]>(
+  "https://jsonplaceholder.typicode.com/users"
 );
 </script>
