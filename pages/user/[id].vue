2a445e4ff6de6888666a45108a6efa45a66b324b
Brenda Daroz <brenda.daroz@gmail.com>
2024-04-21 12:24:11 +0200

    add user details api call

--- pages/user/[id].vue
+++ pages/user/[id].vue
@@ -1,11 +1,22 @@
 <template>
   <article>
     <h1>User profile</h1>
+    <p v-if="pending">Loading...</p>
+    <div v-else>
+      <p>{{ userDetails?.name }}</p>
+      <p>{{ userDetails?.id }}</p>
+    </div>
   </article>
 </template>
 
-<script setup>
-// Fetch user data
+<script setup lang="ts">
+type UserDetails = {
+  id: number;
+  name: string
+}
+const route = useRoute()
+const { data: userDetails, pending, error } = await useFetch<UserDetails>(`https://jsonplaceholder.typicode.com/users/${route.params.id}`)
+console.log(userDetails.value?.id)
 </script>
 
 <style></style>
