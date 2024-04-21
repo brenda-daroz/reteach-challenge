060075f9050d3d6c1a592b3d0867fadd867a364e
Brenda Daroz <brenda.daroz@gmail.com>
2024-04-21 14:12:04 +0200

    create logic to handle open/close menu

--- components/AwesomeHeader.vue
+++ components/AwesomeHeader.vue
@@ -1,5 +1,50 @@
 <template>
-  <header>
-    <h1>Header</h1>
-  </header>
+  <div :class="{ topnav: true, 'is-open': isDropdownOpen }">
+    <header>
+      <h1>Header</h1>
+    </header>
+    <AwesomeNavbar
+      :is-dropdown-open="isDropdownOpen"
+      @toggle-dropdown="toggleDropdown"
+    />
+  </div>
 </template>
+
+<script lang="ts">
+import { ref } from "vue";
+import AwesomeNavbar from "./AwesomeNavbar.vue";
+
+export default {
+  components: {
+    AwesomeNavbar,
+  },
+  setup() {
+    const isDropdownOpen = ref(false);
+
+    const toggleDropdown = () => {
+      isDropdownOpen.value = !isDropdownOpen.value;
+    };
+
+    return {
+      isDropdownOpen,
+      toggleDropdown,
+    };
+  },
+};
+</script>
+
+<style>
+.topnav {
+  background-color: aliceblue;
+  overflow: hidden;
+  padding: 10px;
+  justify-content: space-between;
+  display: flex;
+}
+
+.is-open {
+  position: fixed;
+  width: 100%;
+  box-sizing: border-box;
+}
+</style>
