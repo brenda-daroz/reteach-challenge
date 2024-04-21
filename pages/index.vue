c1ec1b15d8f3297999650248e0ff919ad13d1083
Brenda Daroz <brenda.daroz@gmail.com>
2024-04-21 14:17:54 +0200

    add responsiveness to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,56 @@
 <template>
-  <UsersList />
-  <aside>
-    <h2>Sidebar</h2>
-    <hr />
-    <h3>What is this website?</h3>
-    <h4>Awesome title 4</h4>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita
-      explicabo vero commodi hic voluptates dignissimos non maxime, sunt enim
-      architecto doloribus, minima nobis nemo. Numquam maiores aliquid itaque
-      dolor aliquam!
-    </p>
-    <hr />
-    <h4>Awesome title 4</h4>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita
-      explicabo vero commodi hic voluptates dignissimos non maxime, sunt enim
-      architecto doloribus, minima nobis nemo. Numquam maiores aliquid itaque
-      dolor aliquam!
-    </p>
-    <p>Random text to conclude.</p>
-  </aside>
+  <div class="site-home">
+    <UsersList />
+    <aside class="sidebar">
+      <h2>Sidebar</h2>
+      <hr />
+      <h3>What is this website?</h3>
+      <h4>Awesome title 4</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita
+        explicabo vero commodi hic voluptates dignissimos non maxime, sunt enim
+        architecto doloribus, minima nobis nemo. Numquam maiores aliquid itaque
+        dolor aliquam!
+      </p>
+      <hr />
+      <h4>Awesome title 4</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita
+        explicabo vero commodi hic voluptates dignissimos non maxime, sunt enim
+        architecto doloribus, minima nobis nemo. Numquam maiores aliquid itaque
+        dolor aliquam!
+      </p>
+      <p>Random text to conclude.</p>
+    </aside>
+  </div>
 </template>
+
+<style>
+.site-home {
+  display: flex;
+  flex: 1;
+  width: 100%;
+  box-sizing: border-box;
+}
+section {
+  order: 2;
+  width: 80%;
+}
+
+.sidebar {
+  order: 1;
+  width: 20%;
+}
+
+@media screen and (max-width: 1024px) {
+  .site-home {
+    flex-direction: column;
+  }
+
+  section,
+  .sidebar {
+    order: initial;
+    width: initial;
+  }
+}
+</style>
